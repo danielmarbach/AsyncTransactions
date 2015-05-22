ac5505d3a81f9fdec0163e228cd51959340b72b0
danielmarbach <daniel.marbach@particular.net>
2015-05-22 13:01:49 +0200

    Simple version

---
+++ AsyncTransactions/AsyncTransactions/Database.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Transactions;
+using Newtonsoft.Json;
+
+namespace AsyncTransactions
+{
+    public class Database
+    {
+        List<object> stored = new List<object>();
+        private DatabaseStore store;
+
+        public Database(string storePath)
+        {
+            store = new DatabaseStore(storePath);
+        }
+
+        public void Store(object entity)
+        {
+            stored.Add(entity);            
+        }
+
+        public Task SaveAsync()
+        {
+            return SaveAsync(Transaction.Current);
+        }
+
+        public async Task SaveAsync(Transaction transaction)
+        {
+            if (transaction == null)
+            {
+                await SaveInternalAsync();
+                return;
+            }
+
+            await Task.FromResult(transaction.EnlistVolatile(new SaveResourceManager(SaveInternalAsync), EnlistmentOptions.None));
+        }
+
+        private async Task SaveInternalAsync()
+        {
+            foreach (var o in stored)
+            {
+                using(var stream = new MemoryStream())
+                using (var writer = new JsonTextWriter(new StreamWriter(stream)))
+                {
+                    var serializer = JsonSerializer.Create();
+                    serializer.Serialize(writer, o);
+                    writer.Flush();
+                    stream.Position = 0;
+
+                    await store.AppendAsync(stream);
+
+                    writer.Close();
+                }
+            }
+        }
+
+        public void Close()
+        {
+            store.Close();
+        }
+    }
+
+    public class SaveResourceManager : IEnlistmentNotification
+    {
+        private readonly Func<Task> operation;
+
+        public SaveResourceManager(Func<Task> operation)
+        {
+            this.operation = operation;
+        }
+
+        public void Prepare(PreparingEnlistment preparingEnlistment)
+        {
+            preparingEnlistment.Prepared();
+        }
+
+        public void Commit(Enlistment enlistment)
+        {
+            operation().Wait();
+            enlistment.Done();
+        }
+
+        public void Rollback(Enlistment enlistment)
+        {
+            enlistment.Done();
+        }
+
+        public void InDoubt(Enlistment enlistment)
+        {
+            enlistment.Done();
+        }
+    }
+
+    public class DatabaseStore
+    {
+        private FileStream fileStore;
+
+        public DatabaseStore(string storePath)
+        {
+            fileStore = File.OpenWrite(storePath);
+        }
+
+        public async Task AppendAsync(Stream stream, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await stream.CopyToAsync(fileStore, 4096, cancellationToken);
+            await fileStore.FlushAsync(cancellationToken);
+        }
+
+        public void Close()
+        {
+            fileStore.Close();
+        }
+    }
+
+    internal class ResourceManager
+    {
+        
+    }
+}
---
+++ AsyncTransactions/AsyncTransactions/DatabaseTests.cs
@@ -0,0 +1,89 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Transactions;
+using ApprovalTests;
+using ApprovalTests.Namers;
+using ApprovalTests.Namers.StackTraceParsers;
+using ApprovalTests.Reporters;
+using ApprovalTests.StackTraceParsers;
+using NUnit.Framework;
+
+namespace AsyncTransactions
+{
+    [TestFixture]
+    [UseReporter(typeof(DiffReporter))]
+    public class DatabaseTests
+    {
+        private Database database;
+
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public async Task StoreAsync()
+        {
+            database = new Database("StoreAsync.received.txt");
+
+            for (int i = 0; i < 10; i++)
+            {
+                database.Store(new Customer { Name = "Daniel" + i });
+            }
+            
+            await database.SaveAsync();
+
+            database.Close();
+
+            // Doesn't seem to work properly
+            // Approvals.VerifyFile("StoreAsync.received.txt");
+        }
+
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public async Task StoreAsyncSupportsAmbientTransactionComplete()
+        {
+            database = new Database("StoreAsyncSupportsAmbientTransactionComplete.received.txt");
+
+            for (int i = 0; i < 10; i++)
+            {
+                database.Store(new Customer { Name = "Daniel" + i });
+            }
+
+            using (var tx = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await database.SaveAsync();
+
+                tx.Complete();
+            }
+
+            database.Close();
+
+            Approvals.VerifyFile("StoreAsyncSupportsAmbientTransactionComplete.received.txt");
+        }
+
+        [Test]
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public async Task StoreAsyncSupportsAmbientTransactionRollback()
+        {
+            database = new Database("StoreAsyncSupportsAmbientTransactionRollback.received.txt");
+
+            for (int i = 0; i < 10; i++)
+            {
+                database.Store(new Customer { Name = "Daniel" + i });
+            }
+
+            using (var tx = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await database.SaveAsync();
+            }
+
+            database.Close();
+
+            Approvals.VerifyFile("StoreAsyncSupportsAmbientTransactionRollback.received.txt");
+        }
+
+        private class Customer
+        {
+            public string Name { get; set; }
+        }
+    }
+}
